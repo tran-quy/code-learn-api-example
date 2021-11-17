7b78b661dd0ac6f201d6946e7d88ee6abe55ead8
Quy Tran <tran.quy@marketenterprise.vn>
2021-11-17 19:30:03 +0700

    ok

---
+++ pages/api/info.js
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function infoAPI(req, res) {
+    res.status(200).json({
+        name: 'Phung Quy'
+    })
+}
